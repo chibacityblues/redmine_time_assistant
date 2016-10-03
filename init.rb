58f6631c9254624e58d08b821686c5c9e951761e
Oliver Lösch <oliver@loesch.io>
2016-10-04 00:14:16 +0200

    Create plugin skeleton

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_time_assistant do
+  name 'Redmine Time Assistant plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
